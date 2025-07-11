58840961a46cfac168c896a03f840e8e7f66dc85
Grazielle Gualter <grazi.gualter@gmail.com>
2025-07-10 23:32:42 -0300

    Criação Conta Corrente e Conta Poupança

--- Menu.ts
+++ Menu.ts
@@ -1,6 +1,8 @@
 import readlinesync = require("readline-sync");
 import { colors } from './src/util/Colors';
 import { Conta } from './src/model/Conta';
+import { ContaCorrente } from './src/model/ContaCorrente';
+import { ContaPoupanca } from './src/model/ContaPoupanca';
 
 export function main() {
 
@@ -13,10 +15,24 @@ export function main() {
     conta.depositar(5000);
     conta.visualizar();
 
+    const contacorrente: ContaCorrente = new ContaCorrente(2, 123, 1, "Mariana", 15000, 1000);
+    contacorrente.visualizar();
+    contacorrente.sacar(2000);
+    contacorrente.visualizar();
+    contacorrente.depositar(1000);
+    contacorrente.visualizar();
+
+    const contapoupanca: ContaPoupanca = new ContaPoupanca(3, 123, 2, "Victor", 1000, 10);
+    contapoupanca.visualizar();
+    contapoupanca.sacar(200);
+    contapoupanca.visualizar();
+    contapoupanca.depositar(1000);
+    contapoupanca.visualizar();
+
     while (true) {
 
         console.log(colors.bg.black, colors.fg.yellow,
-            "*****************************************************");
+                    "*****************************************************");
         console.log("                                                     ");
         console.log("               🏦 BANCO DA LAMENTACAO                ");
         console.log("                                                     ");
